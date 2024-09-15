3fd72b638276d1b8d47b757e3c851d496391d741
kitasolusimarketingmu <118248295+kitasolusimarketingmu@users.noreply.github.com>
2024-09-15 21:41:37 +0700

    Update popupnotifydavo88.js

--- javascript/popupnotifydavo88.js
+++ javascript/popupnotifydavo88.js
@@ -1,4 +1,4 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js';
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js';
 import { getMessaging, getToken, onMessage } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-messaging.js';
 
 
