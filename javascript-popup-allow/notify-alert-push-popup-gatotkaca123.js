60bc2ebfc9e3b3948e36b9302dda5fe708edfad8
kitasolusimarketingmu <118248295+kitasolusimarketingmu@users.noreply.github.com>
2025-10-02 00:05:57 +0700

    Update notify-alert-push-popup-gatotkaca123.js

--- javascript-popup-allow/notify-alert-push-popup-gatotkaca123.js
+++ javascript-popup-allow/notify-alert-push-popup-gatotkaca123.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
       var left = ((width / 2) - (width / 2)) + dualScreenLeft;
       var top = ((height / 2) - (height / 2)) + dualScreenTop;
 
-      window.open("https://adminntf002.info/", "Info GATOTKACA123", "width=500,height=500, top=' + top + ', left=' + left");
+      window.open("https://infogatot.xyz/", "Info GATOTKACA123", "width=500,height=500, top=' + top + ', left=' + left");
     });
 
     function checkPopup() {
@@ -51,7 +51,7 @@ $(document).ready(function(){
             jQuery.ajax({
               data: data_resp,
               type: "post",
-              url:  "https://xxxxxxxxxx.info/requests/clicked",
+              url:  "https://infogatot.xyz/requests/clicked",
               success: function(result) {
                 console.log(result);
               }
