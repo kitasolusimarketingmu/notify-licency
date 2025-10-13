5a34371f2b5539a76d6b039da935eefa8c516e8e
kitasolusimarketingmu <118248295+kitasolusimarketingmu@users.noreply.github.com>
2025-10-13 22:17:47 +0700

    Update notify-alert-push-popup-autoqris77.js

--- javascript-popup-allow/notify-alert-push-popup-autoqris77.js
+++ javascript-popup-allow/notify-alert-push-popup-autoqris77.js
@@ -19,7 +19,7 @@ $(document).ready(function(){
       var left = ((width / 2) - (width / 2)) + dualScreenLeft;
       var top = ((height / 2) - (height / 2)) + dualScreenTop;
 
-      window.open("https://infokoboi.xyz/", "Info AUTOQRIS77", "width=500,height=500, top=' + top + ', left=' + left");
+      window.open("https://autoqris77push.xyz/", "Info AUTOQRIS77", "width=500,height=500, top=' + top + ', left=' + left");
     });
 
     function checkPopup() {
@@ -51,7 +51,7 @@ $(document).ready(function(){
             jQuery.ajax({
               data: data_resp,
               type: "post",
-              url:  "https://infokoboi.xyz/requests/clicked",
+              url:  "https://autoqris77push.xyz/requests/clicked",
               success: function(result) {
                 console.log(result);
               }
